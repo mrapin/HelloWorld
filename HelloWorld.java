d3ad68cfd63e583e4ebe54fb370b80eed8ebb955
mrapin <35694926+mrapin@users.noreply.github.com>
2018-01-29 14:49:09 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-		System.out.println("TEST OK !!!!!");
+	System.out.println("TEST OK !!!!!");
+	System.out.println("Nouvelle ligne de test");
     }
 
 }
